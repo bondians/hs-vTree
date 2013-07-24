a06e0be39d6146da314d6a5f097247ff4a2c29a4
James Cook <mokus@deepbondi.net>
2013-07-23 22:38:04 -0700

    hacky update for experimental xbee version

--- src/VTree.hs
+++ src/VTree.hs
@@ -1,53 +1,63 @@
-module VTree where
+module VTree
+    ( Addr(..)
+    , module VTree
+    ) where
 
-import Control.Applicative
 import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Monad
+import Data.Serialize
 import Data.Word
-import System.IO
 import System.Serial
 import System.Posix.Terminal
+import Network.Protocol.XBee
+import Network.Protocol.XBee.Series1
 
-data VTree = VTree !Handle !(MVar Word8)
-type Level = Word8
+data VTree = VTree !XBee !ThreadId !(TVar (Word16, Word16, Word16))
+type Level = Word16
 
 data Response = OK | Fail !String
     deriving (Eq, Ord, Show)
 
+sender :: XBee -> TVar (Word16, Word16, Word16) -> IO ()
+sender h t = go =<< atomically (readTVar t)
+    where
+        go prev = do
+            next <- atomically $ do
+                next <- readTVar t
+                when (next == prev) retry
+                return next
+            
+            send h (Addr16 2) next
+            threadDelay (round (1e6 / 30 :: Double))
+            go next
+
 -- note: use the 'cu' device on Mac OS, not the 'tty'
-openVTreeLed port addr = VTree
-    <$> openSerial port B38400 8 One NoParity NoFlowControl
-    <*> newMVar addr
+openVTree :: FilePath -> IO VTree
+openVTree port = do
+    x <- runXBee True =<< openSerial port B38400 8 One NoParity NoFlowControl
+    
+    t <- newTVarIO (0,0,0)
+    tid <- forkIO (sender x t)
+    
+    return (VTree x tid t)
 
-closeVTree (VTree h _) = hClose h
+closeVTree :: VTree -> IO ()
+closeVTree (VTree x t _) = do
+    killThread t
+    closeXBee x
 
-parseResponse "Ok" = OK
-parseResponse other = Fail other
-readResponse (VTree h _) = parseResponse <$> hGetLine h
+setRGB :: VTree -> Addr -> Double -> Double -> Double -> IO ()
+setRGB h addr r g b = setRGB16 h addr (f r) (f g) (f b)
+    where
+        clip lo hi = max lo . min hi
+        f x = truncate (clip 0 0xFFFF (0x10000 * x))
 
-sendCommand' (VTree h _) = hPutStrLn h . unwords
-sendCommand vTree cmd = do
-    sendCommand' vTree cmd
-    readResponse vTree
+-- TODO: handle addr, currently it's ignored
+setRGB16 :: VTree -> Addr -> Level -> Level -> Level -> IO ()
+setRGB16 (VTree _ _ t) _addr r g b = atomically (writeTVar t (r, g, b))
 
-setCmd :: String -> VTree -> Level -> IO ()
-setCmd cmd vTree@(VTree _ a) level = do
-    addr <- readMVar a
-    sendCommand' vTree [cmd, show addr, show level]
-
-data Color = Red | Yellow | Green | Blue
-    deriving (Eq, Ord, Read, Show, Enum, Bounded)
-
-set Red    = setCmd "setRed"
-set Yellow = setCmd "setYellow"
-set Green  = setCmd "setGreen"
-set Blue   = setCmd "setBlue"
-
-setAll vt level = sequence_
-    [ set color vt level
-    | color <- [minBound .. maxBound]
-    ]
-
-setRYGB :: VTree -> Word8 -> Word8 -> Word8 -> Word8 -> IO ()
-setRYGB vt@(VTree _ a) r y g b = do
-    addr <- readMVar a
-    sendCommand' vt ["setRYGB", show addr, show r, show y, show g, show b]
+send :: XBee -> Addr -> (Word16, Word16, Word16) -> IO ()
+send x addr (r, g, b) = writeXBee x $
+    TxFrame (Tx 0 (Just txDisableAck) addr msg)
+    where msg = runPut (mapM_ putWord16be [b,g,r])
