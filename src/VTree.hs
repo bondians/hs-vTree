bc988818ee57c4b68388376ad3d7bcd59fe966c7
James Cook <mokus@deepbondi.net>
2014-06-05 21:09:06 -0700

    temporary changes to work with dave's prototype...
    need to merge S1 and S2 versions, but don't have time/energy right now.

--- src/VTree.hs
+++ src/VTree.hs
@@ -7,23 +7,24 @@ import Data.Serialize
 import Data.Word
 import System.Serial
 import System.Posix.Terminal
+import System.IO
 import Network.Protocol.XBee
-import Network.Protocol.XBee.Series2
+import Network.Protocol.XBee.Series1
 
-data VTree = VTree !(XBee Series2) !ThreadId !(TVar (Word16, Word16, Word16))
+data VTree = VTree !(XBee Series1) !ThreadId !(TVar (Word16, Word16, Word16))
 type Level = Word16
 
 data Response = OK | Fail !String
     deriving (Eq, Ord, Show)
 
-type Addr = Word64
+-- type Addr = Word64
 
 prototype :: Addr
 prototype =
-    -- Addr16 2
-    0x13a20040b087b0
+    Addr16 2
+    -- 0x13a20040b087b0
 
-sender :: XBee Series2 -> TVar (Word16, Word16, Word16) -> IO ()
+sender :: XBee Series1 -> TVar (Word16, Word16, Word16) -> IO ()
 sender h t = go =<< atomically (readTVar t)
     where
         go prev = do
@@ -36,15 +37,24 @@ sender h t = go =<< atomically (readTVar t)
             threadDelay (round (1e6 / 30 :: Double))
             go next
 
+foo port = do
+    p <- openSerial port B9600  8 One NoParity NoFlowControl
+    
+    hClose p
+
 -- note: use the 'cu' device on Mac OS, not the 'tty'
 openVTree :: FilePath -> IO VTree
 openVTree port = do
+    foo port
     x <- runXBee True =<< openSerial port B38400 8 One NoParity NoFlowControl
     
     t <- newTVarIO (0,0,0)
     tid <- forkIO (sender x t)
     
     return (VTree x tid t)
+
+bar :: Num a => a
+bar = 42
 
 closeVTree :: VTree -> IO ()
 closeVTree (VTree x t _) = do
@@ -61,7 +71,9 @@ setRGB h addr r g b = setRGB16 h addr (f r) (f g) (f b)
 setRGB16 :: VTree -> Addr -> Level -> Level -> Level -> IO ()
 setRGB16 (VTree _ _ t) _addr r g b = atomically (writeTVar t (r, g, b))
 
-send :: XBee Series2 -> Addr -> (Word16, Word16, Word16) -> IO ()
+send :: XBee Series1 -> Addr -> (Word16, Word16, Word16) -> IO ()
 send x addr (r, g, b) = writeXBee x $
-    TxFrame (Tx 0 addr 0xFFFE 0 Nothing msg)
+    --TxFrame (Tx 0 addr 0xFFFE 0 Nothing msg)
+        TxFrame (Tx 0 (Just txDisableAck) addr msg)
+
     where msg = runPut (mapM_ putWord16be [r,g,b])
